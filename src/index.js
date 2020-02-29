f255abf2e03de8fd71b36e49effcb4a3b6652083
davinchick <57950327+davinchick@users.noreply.github.com>
2020-02-29 17:09:53 +0300

    Add: solved wonderful task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let i = 1;
+    const loveObj = (arr) =>
+        Object.assign({}, ...arr.map(item => ({[i++]: item})));
+    let lovers = loveObj(preferences);
+
+    let countTriangles = 0;
+
+    for (let j = 0; j < preferences.length; j++) {
+        let man = preferences[j];
+        let man2 = lovers[man];
+        let man3 = lovers[man2];
+
+        if ((man3 === (j + 1)) && man !== man2) {
+            countTriangles++;
+            delete lovers[j + 1];
+            delete lovers[man];
+            delete lovers[man2];
+        }
+    }
+
+    return countTriangles;
 };
